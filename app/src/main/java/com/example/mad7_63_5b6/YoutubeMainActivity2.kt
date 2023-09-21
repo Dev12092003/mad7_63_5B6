41cdacddaa05752e8928bca5e8db7e20053f8ec3
Dev <dev.12092003@gmail.com>
2023-09-21 12:33:15 +0530

    commite

---
+++ app/src/main/java/com/example/mad7_63_5b6/YoutubeMainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.mad7_63_5b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YoutubeMainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube_main2)
+    }
+}
