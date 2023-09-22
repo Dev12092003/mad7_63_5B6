1599f0cd40c66bb2d7c1fb3176c5157c9f373210
Dev <dev.12092003@gmail.com>
2023-09-22 12:35:41 +0530

    commite

--- app/src/main/java/com/example/mad7_63_5b6/MainActivity.kt
+++ app/src/main/java/com/example/mad7_63_5b6/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.mad7_63_5b6
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import com.example.mad7_63_5b6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initVideoPlayer()
+            binding.webnextbtn.setOnClickListener{
+                Intent(this,YoutubeMainActivity2::class.java).also { startActivity(it) }
+
+        }
     }
+    private fun initVideoPlayer(){
+        val mediaController=MediaController(this)
+        val uri:Uri= Uri.parse("android.resource://"+packageName+"/"+R.raw.thestoryoflight)
+        binding.videoview1.setVideoURI(uri)
+        binding.videoview1.requestFocus()
+        binding.videoview1.start()
+    }
+
 }
--- app/src/main/java/com/example/mad7_63_5b6/YoutubeMainActivity2.kt
+++ app/src/main/java/com/example/mad7_63_5b6/YoutubeMainActivity2.kt
@@ -1,11 +1,28 @@
 package com.example.mad7_63_5b6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import com.example.mad7_63_5b6.databinding.ActivityYoutubeMain2Binding
 
 class YoutubeMainActivity2 : AppCompatActivity() {
+    private lateinit var binding:ActivityYoutubeMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube_main2)
+        binding=ActivityYoutubeMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initYoutubePlayer()
+        binding.backbtn.setOnClickListener{
+            Intent(this,MainActivity::class.java).also { startActivity(it) }
+        }
+    }
+    private val youtubeID= "v=r-9uGG_-fp0&t=37s"
+    private fun initYoutubePlayer(){
+        val websettings:WebSettings=binding.webviwe.settings
+        websettings.javaScriptEnabled=true
+        websettings.loadWithOverviewMode=true
+        websettings.useWideViewPort=true
+        binding.webviwe.loadUrl("https://www.youtube.com/embed/$youtubeID")
     }
 }
